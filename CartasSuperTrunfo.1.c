937c12087ecb311344be3d1a64e21d54e92cae76
Tatah <thaysamoura0001@gmail.com>
2025-04-23 00:18:23 -0300

    Update CartasSuperTrunfo.1.c

--- CartasSuperTrunfo.1.c
+++ CartasSuperTrunfo.1.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+
 int main (){
- char Estado; 
- char Código da Carta;
- int População;
- float area em km²;
- float Pib;
- Int Números de pontos tuísticos;
+    printf("Olá, jogador(a), sejam bem vindos(as) ao jogo Cartas Super Trunfo, registre aqui os dados da sua carta!")
+
+    //variavies para a primeira carta
+ char Estado [40] = "R";
+ char Código da Carta[30]= R02;
+ char Nome da Cidade [30]= "Rio de Janeiro";
+ int População 211.223;
+ float area 43 750,425 km²;
+ float Pib 300.50 bilhões de reais;
+ int Números de pontos tuísticos 58;
+ 
+  
  
 
 
