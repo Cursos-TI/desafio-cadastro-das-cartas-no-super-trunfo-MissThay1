d02f9e5697bd7b641412081a35c841d2dec38535
Tatah <thaysamoura0001@gmail.com>
2025-04-22 21:28:15 -0300

    Update CartasSuperTrunfo.1.c

--- CartasSuperTrunfo.1.c
+++ CartasSuperTrunfo.1.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main (){
- char Estado 
- char Código da Carta
- int População
- float area em km²
- float Pib
- Int Números de pontos tuísticos
+ char Estado; 
+ char Código da Carta;
+ int População;
+ float area em km²;
+ float Pib;
+ Int Números de pontos tuísticos;
  
 
 
