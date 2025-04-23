9f77cfaa4fa86a736d556654fa20e70f6a87fbaf
Tatah <thaysamoura0001@gmail.com>
2025-04-22 21:20:57 -0300

    Create CartasSuperTrunfo.1.c
    Projeto nivel inciante:

---
+++ CartasSuperTrunfo.1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main (){
+ char Estado 
+ char Código da Carta
+ int População
+ float area em km²
+ float Pib
+ Int Números de pontos tuísticos
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
